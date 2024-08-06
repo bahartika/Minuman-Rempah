b06a1a42c619b412906a19fc9317af34c5fa68dc
bahartika <bahartikasatria271@gmail.com>
2024-08-06 08:42:28 +0700

    du-musik website

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDODjEdDfq15kpZgEJ3Vp5Xmr0ocPXU-iQ",
+  authDomain: "du-music-28cb0.firebaseapp.com",
+  projectId: "du-music-28cb0",
+  storageBucket: "du-music-28cb0.appspot.com",
+  messagingSenderId: "236990387183",
+  appId: "1:236990387183:web:f8848ab692911fc06f1c1a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
